e5a75c20085d022ca60f5f1659e2774440a58a30
KuchumovaSvetlana <kuchumova@nwtn-group.com>
2021-09-11 23:52:32 +0500

    upd POST-request

--- src/test/java/PostRequest.java
+++ src/test/java/PostRequest.java
@@ -7,31 +7,33 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.json.JSONObject;
+
 public class PostRequest {
 
     @Test
     public void postWithJsonBody() {
         RestAssured.baseURI = "https://httpbin.org";
 
+        JSONObject requestBody = new JSONObject()
+                .put("name", "test name")
+                .put("age", 18)
+                .put("hobby", "sport");
         Response response = given()
-                .body("{\n" +
-                        "    \"name\": \"test name\",\n" +
-                        "    \"age\": 18,\n" +
-                        "    \"hobby\": \"sport\"\n" +
-                        "}").
-        when()
-                .post("/post").
-        then()
+                .body(requestBody.toString())
+                .when()
+                .post("/post")
+                .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
                 .body("data", containsString("test name"))
-                .body("headers.Content-Length", equalTo("64"))
+                .body("headers.Content-Length", equalTo("45"))
                 .extract().response();
 
         System.out.println("POST REQUEST");
         System.out.println("Response body from POST request:");
         response.prettyPrint();
-        String origin= response.path("origin");
+        String origin = response.path("origin");
         System.out.println("origin: " + origin);
     }
 }
